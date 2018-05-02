c4555f17ad12ca9d560c86f65cda04f546fd7b00
Jianyang Tang <JianyangTang@m-wise.nl>
2018-05-02 15:42:04 +0200

    The First vesrion of the app, the one fowled youtube

---
+++ pages/index/index.js
@@ -0,0 +1,83 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    Bills: [{
+      id: 1,
+      title: '应季鲜果',
+      count: 1,
+      image: 'http://www.emtech.in/wp-content/uploads/2017/03/pic.jpg'
+    },
+    {
+      id: 2,
+      title: '鲜果',
+      count: 2,
+      image: 'http://www.emtech.in/wp-content/uploads/2017/03/pic.jpg'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.showToast({
+      title: '唤醒中...',
+      icon:"loading",
+      duration:1000
+    })
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
